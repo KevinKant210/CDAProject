5e79051ba324f30b73c3be6808d38a8a5456479a
Kevin Desktop <kevinkant@knights.ucf.edu>
2021-11-13 11:52:03 -0500

    Adding Comments on functions required proccesses

--- project.c
+++ project.c
@@ -87,9 +87,13 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 {
     //implement some form of bit masking to extract the bits from istruction
     
-    //R-format  OP (6 bit) - RS 5 bit - rt 5bit - rd 5 bit - shamt 5 bit - funct 6 bit
-    //I-format op 6 bit - rs 5 bit - rt 5 bit - address/immediate 16 bits
-    //J format op 6bits - target address 26 bits
+    //R-format  OP (6 bit) - RS 5 bit - rt 5bit - rd 5 bit - shamt 5 bit - funct 6 bit // 0 opccode
+    //I-format op 6 bit - rs 5 bit - rt 5 bit - address/immediate 16 bits   //opcode > 3
+    //J format op 6bits - target address 26 bits    //opcode of 2 or 3
+    /*
+        Partition instruction into several parts (op, r1, r2, r3, funct, offset, jsec). 
+        2.  Read line 41 to 47 of spimcore.c for more information.
+    */
     
 }
 
@@ -98,7 +102,20 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
-{
+{   
+    /*
+        Decode the instruction using the opcode (op). 
+2.  Assign the values of the control signals to the variables in the structure controls 
+(See spimcore.h file). 
+The meanings of the values of the control signals: 
+For MemRead, MemWrite or RegWrite, the value 1 means that enabled, 0 
+means that disabled, 2 means “don’t care”. 
+For RegDst, Jump, Branch, MemtoReg or ALUSrc, the value 0 or 1 indicates the 
+selected path of the multiplexer; 2 means “don’t care”. 
+The following table shows the meaning of the values of ALUOp. 
+ 
+3.  Return 1 if a halt condition occurs; otherwise, return 0. 
+    */
 
 }
 
@@ -106,7 +123,8 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    //Read the registers addressed by r1 and r2 from Reg, and write the read values 
+    //to data1 and data2 respectively.
 }
 
 
@@ -114,21 +132,37 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    //Assign the sign-extended value of offset to extended_value. 
 }
 
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-
+        /*
+            Apply ALU operations on data1, and data2 or extended_value (determined by 
+    ALUSrc). 
+    2.  The operation performed is based on ALUOp and funct. 
+    3.  Apply the function ALU(...). 
+    4.  Output the result to ALUresult. 
+    5.  Return 1 if a halt condition occurs; otherwise, return 0.
+        */
 }
 
 /* Read / Write Memory */
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
+        /*
+            1.  Use the value of MemWrite or MemRead to determine if a memory write 
+    operation or memory read operation or neither is occurring. 
+    2.  If reading from memory, read the content of the memory location addressed by 
+    ALUresult to memdata. 
 
+    3.  If writing to memory, write the value of data2 to the memory location 
+    addressed by ALUresult. 
+    4.  Return 1 if a halt condition occurs; otherwise, return 0.
+        */
 }
 
 
@@ -136,6 +170,9 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+    /*
+        1.  Write the data (ALUresult or memdata) to a register (Reg) addressed by r2 or r3. 
+    */
 
 }
 
@@ -143,6 +180,7 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
+    //1.  Update the program counter (PC). 
     //to access proper memory index need to divide pc by four 
 
     
